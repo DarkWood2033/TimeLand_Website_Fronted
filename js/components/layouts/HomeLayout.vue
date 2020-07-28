b48697390baca06d999331cb828612aaa802e312
DarkWood2033 <bariev.ilnarbi@gmail.com>
2020-07-28 21:59:22 +0300

    finish home router

---
+++ js/components/layouts/HomeLayout.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="main-layout">
+        <v-header></v-header>
+
+        <div id="content">
+            <div class="container">
+                <router-view></router-view>
+            </div>
+        </div>
+
+        <v-footer></v-footer>
+
+        <v-dialog></v-dialog>
+
+        <v-notification></v-notification>
+
+        <v-loading></v-loading>
+    </div>
+</template>
+
+<script>
+    import HeaderComponent from "./components/HeaderComponent";
+    import TitleComponent from "./components/TitleComponent";
+    import FooterComponent from "./components/FooterComponent";
+    import DialogComponent from "./components/DialogComponent";
+    import NotificationComponent from "./components/NotificationComponent";
+    import LoadingComponent from "./components/LoadingComponent";
+
+    export default {
+        name: "MainLayout",
+        components: {
+            vHeader: HeaderComponent,
+            vTitle: TitleComponent,
+            vFooter: FooterComponent,
+            vDialog: DialogComponent,
+            vNotification: NotificationComponent,
+            vLoading: LoadingComponent
+        }
+    }
+</script>
+
+<style>
+
+    body, html{
+        height: 100%;
+    }
+
+</style>
+<style lang="scss" scoped>
+    @import "@sass/variable";
+
+    div.main-layout{
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+    }
+
+    #content {
+        flex: 1 0 auto;
+        padding-bottom: 30px;
+        padding-top: 30px;
+    }
+    #footer {
+        flex: 0 0 auto;
+    }
+    #title {
+        flex: 0 0 auto;
+    }
+    #header {
+        flex: 0 0 auto;
+    }
+
+</style>
--- js/routes/main/HomeRouter.vue
+++ js/routes/main/HomeRouter.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>Home</div>
+    <div>
+        <div class="row justify-content-center align-items-center" style="height: calc(100vh - 200px)">
+            <div class="col-md-6">
+                <p class="h1 text_center text_bold">
+                    Добро пожаловать <span class="text_secondary">Time</span><span class="text_primary">Land</span>
+                </p>
+                <p class="h2 text_center mt-2">
+                    Вы ощутились на планете "Время". Здесь обитают все инструменты связанное с временим.
+                </p>
+            </div>
+            <div class="col-md-6">
+                <img
+                    class="illustration"
+                    src="/site/img/header_slider.svg"
+                    alt="TimeLand logo"
+                >
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +27,11 @@
 </script>
 
 <style scoped>
-
+    img.illustration{
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 100%;
+        max-height: 550px;
+    }
 </style>
